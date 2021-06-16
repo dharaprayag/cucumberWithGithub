85162dc11d150a21d9be3e30f67fee3c241cf7a9
dharaprayagraj <dharaprayagraj@gmail.com>
2021-06-16 09:25:06 -0400

    stepDefination file Added

---
+++ src/test/java/stepDefinations/stepDefination.java
@@ -0,0 +1,32 @@
+package stepDefinations;
+
+import io.cucumber.java.PendingException;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+
+public class stepDefination 
+{
+	@Given("^User is on NetBanking landing page$")
+    public void user_is_on_netbanking_landing_page() throws Throwable {
+        throw new PendingException();
+    }
+
+    @When("^User login into application with username and password$")
+    public void user_login_into_application_with_username_and_password() throws Throwable {
+        throw new PendingException();
+    }
+
+    @Then("^Home page is populated$")
+    public void home_page_is_populated() throws Throwable {
+        throw new PendingException();
+    }
+
+    @And("^Cards are displayed$")
+    public void cards_are_displayed() throws Throwable {
+        throw new PendingException();
+    }
+
+}
