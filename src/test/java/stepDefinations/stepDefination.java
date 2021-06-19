565fbdc4aa9137fa358f50e4a9f9819fdd8cdcb0
dharaprayagraj <dharaprayagraj@gmail.com>
2021-06-19 07:44:58 -0400

    modified in pom.xml and stepDefination files

--- src/test/java/stepDefinations/stepDefination.java
+++ src/test/java/stepDefinations/stepDefination.java
@@ -1,32 +1,34 @@
 package stepDefinations;
 
-import io.cucumber.java.PendingException;
-import io.cucumber.java.en.And;
-import io.cucumber.java.en.Given;
-import io.cucumber.java.en.Then;
-import io.cucumber.java.en.When;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.When;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.And;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
 
+@RunWith(Cucumber.class)
+public class stepDefination {
 
-public class stepDefination 
-{
-	@Given("^User is on NetBanking landing page$")
-    public void user_is_on_netbanking_landing_page() throws Throwable {
-        throw new PendingException();
+    @Given("^User is on NetBanking landing page$")
+    public void user_is_on_netbanking_landing_page() throws Throwable 
+    {
+        System.out.println("User is on NetBanking landing page");
     }
 
     @When("^User login into application with username and password$")
     public void user_login_into_application_with_username_and_password() throws Throwable {
-        throw new PendingException();
+    	System.out.println("User login into application with username and password");
     }
 
     @Then("^Home page is populated$")
     public void home_page_is_populated() throws Throwable {
-        throw new PendingException();
+    	System.out.println("Home page is populated");
     }
 
     @And("^Cards are displayed$")
     public void cards_are_displayed() throws Throwable {
-        throw new PendingException();
+    	System.out.println("Home page is populated");
     }
 
 }
